c91d7e713d52435d169d7690ee969706b4a5b5af
DMondi <tways.d@gmail.com>
2016-02-05 18:59:10 +0200

    Create MainActivity and Layout

---
+++ app/src/main/java/com/ponomarev/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ponomarev.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.ponomarev.remindme.R;
+
+/**
+ * Created by ponomarev on 05.02.2016.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
