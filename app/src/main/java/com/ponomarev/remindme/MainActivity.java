a271e1feb25448bf0872f612359a04249f99f381
DMondi <tways.d@gmail.com>
2016-02-07 18:51:46 +0200

    Create MainActivity and Layout

--- app/src/main/java/com/ponomarev/remindme/MainActivity.java
+++ app/src/main/java/com/ponomarev/remindme/MainActivity.java
@@ -2,6 +2,8 @@ package com.ponomarev.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.ponomarev.remindme.R;
 
@@ -10,9 +12,27 @@ import com.example.ponomarev.remindme.R;
  */
 public class MainActivity extends Activity{
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+        
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
