60b315396dc4523aa70a2617d34f53efae4568b4
DMondi <tways.d@gmail.com>
2016-02-08 19:36:46 +0200

    Drawer Layout

--- app/src/main/java/com/ponomarev/remindme/MainActivity.java
+++ app/src/main/java/com/ponomarev/remindme/MainActivity.java
@@ -2,27 +2,41 @@ package com.ponomarev.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.ponomarev.remindme.R;
+import com.ponomarev.remindme.adapter.TabsPagerFragmentAdapter;
 
 /**
  * Created by ponomarev on 05.02.2016
  */
 public class MainActivity extends AppCompatActivity{
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(LAYOUT);
+
         initToolbar();
-        
+        initNavigationView();
+
+        initTabs();
+
+
     }
 
     private void initToolbar() {
@@ -36,5 +50,16 @@ public class MainActivity extends AppCompatActivity{
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+    }
+
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layuot);
     }
 }
